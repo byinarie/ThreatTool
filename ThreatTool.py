0916a542310180e25e4ae703d7161c9593665441
PJ <prueve@speartip.com>
2023-02-21 16:46:21 -0600

    testing

---
+++ ThreatTool.py
@@ -0,0 +1,70 @@
+import requests
+import json
+import click
+import configparser
+
+@click.command()
+@click.option('--payload', required=True, help='Path to the payload file to scan')
+@click.option('--config', default='config.ini', help='Path to configuration file')
+def scan_file(payload, config):
+    # read the API keys from the configuration file
+    try:
+        config_parser = configparser.ConfigParser()
+        config_parser.read(config)
+        antiscan_api_key = config_parser.get('antiscan', 'api_key')
+        virustotal_api_key = config_parser.get('virustotal', 'api_key')
+        hybrid_analysis_api_key = config_parser.get('hybrid-analysis', 'api_key')
+    except configparser.Error as e:
+        click.echo('Error reading configuration file: {}'.format(e))
+        return
+
+    # make a request to antiscan.me's API to upload the file
+    antiscan_url = "https://antiscan.me/api/v3/file/scan"
+    files = {"file": open(payload, "rb")}
+    headers = {"apikey": antiscan_api_key}
+    response = requests.post(antiscan_url, files=files, headers=headers)
+
+    # check if the request was successful
+    if response.status_code != 200:
+        click.echo("Error: file upload to antiscan.me failed.")
+        click.echo(response.text)
+        return
+
+    # parse the JSON response and print out the scan results from antiscan.me
+    antiscan_result = json.loads(response.text)
+    if antiscan_result["success"]:
+        click.echo("Antiscan.me scan results for file {}: ".format(payload))
+        click.echo("Detected: {}".format(antiscan_result["detected"]))
+        click.echo("Result: {}".format(antiscan_result["result"]))
+    else:
+        click.echo("Error: scan failed on antiscan.me.")
+        click.echo(antiscan_result["message"])
+        return
+
+    # make a request to VirusTotal's API to get scan results by hash
+    vt_url = "https://www.virustotal.com/api/v3/files/{}"
+    headers = {"x-apikey": virustotal_api_key}
+    sha256 = antiscan_result["hashes"]["sha256"]
+    params = {"relationships": "last_analysis_results"}
+    response = requests.get(vt_url.format(sha256), headers=headers, params=params)
+
+    # check if the request was successful
+    if response.status_code != 200:
+        click.echo("Error: getting scan results from VirusTotal failed.")
+        click.echo(response.text)
+        return
+
+    # parse the JSON response and print out the scan results from VirusTotal
+    vt_result = json.loads(response.text)
+    if vt_result["data"]["attributes"]["last_analysis_stats"]["malicious"] > 0:
+        click.echo("VirusTotal scan results for file {}: ".format(payload))
+        click.echo("Detections: {}".format(vt_result["data"]["attributes"]["last_analysis_stats"]["malicious"]))
+        for vendor, result in vt_result["data"]["attributes"]["last_analysis_results"].items():
+            if result["category"] == "malicious":
+                click.echo("{}: {}".format(vendor, result["result"]))
+    else:
+        click.echo("VirusTotal scan results for file {}: No detections found.".format(payload))
+
+    # make a request to Hybrid Analysis to upload the file hash
+    hybrid_analysis_url = "https://www.hybrid-analysis.com/api/v2/quick-scan"
+    headers = {"user-agent": "Falcon Sandbox", "api-key": hybrid_analysis_api
